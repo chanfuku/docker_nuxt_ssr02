ca2feefb0d12dc8e76a44d846c70eebbd718da0d
chanfuku <koheiebato@gmail.com>
2021-09-11 20:23:41 +0900

    ページ遷移の挙動検証

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <Tutorial/>
+  <div>
+    <p>▼SSR(Server Side Renderging)ページ遷移</p>
+    <p><a href="/test">aタグでページ遷移</a></p>
+    <p><button @click="next">window.location.hrefでページ遷移</button></p>
+    <p>▼SPA(Client Side Renderging)ページ遷移</p>
+    <p><NuxtLink to="/test">NuxtLinkでページ遷移</NuxtLink></p>
+    <p><button @click="push">$router.pushでページ遷移</button></p>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
 
-export default Vue.extend({})
+export default {
+  fetch() {
+    console.log('index.vue')
+  },
+  methods: {
+    push() {
+      this.$router.push('/test')
+    },
+    next() {
+      window.location.href = '/test'
+    },
+  },
+}
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+export const state = () => ({
+  images: []
+})
+
+export const mutations = {
+  ADD_IMAGE(state, imgSrc) {
+    state.images.push(imgSrc)
+  }
+}
+
+export const actions = {
+  async fetchRandomDog ({ commit }) {
+    const resJson = await this.$axios.$get('https://dog.ceo/api/breeds/image/random')
+    if (resJson.status === 'success') {
+      commit('ADD_IMAGE', resJson.message)
+    }
+  },
+}
